b1f74b3832caff08ee633db1507f6652bb4e6ae4
iguinn13 <igoor182@hotmail.com>
2021-01-14 13:56:26 -0300

    Incrementando a classe Tabela

--- Tabela.js
+++ Tabela.js
@@ -4,6 +4,14 @@ class Tabela {
         array.shift();
         this.linhas = array;
     }
+
+    get ContadorDeLinhas(){
+        return this.linhas.length;
+    }
+
+    get ContadorDeColunas(){
+        return this.cabecalho.length;
+    }
 }
 
 module.exports = Tabela;
